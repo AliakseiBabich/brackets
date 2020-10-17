75e420d03c53e5392e9e9d418814ef4936600f10
Aliaksei Babich <lex.gmx@gmail.com>
2020-10-17 13:57:09 +0300

    init: create a necessary function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+module.exports = function check (str, bracketsConfig) {
+    const elementsStack = [];
+    const brackets = Object.fromEntries(bracketsConfig);
+    console.log(brackets);
+    for (let element of str.split``) {
+        if (elementsStack[0] === element) {
+            elementsStack.shift();
+        } else if (brackets[element]) {
+            elementsStack.unshift(brackets[element]);
+        } else {
+            return false;
+        }
+    }
+
+    return elementsStack.length === 0;
+};
